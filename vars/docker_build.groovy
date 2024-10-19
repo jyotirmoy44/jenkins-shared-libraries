fe94f09a155e6352e6c0528c109b3ece4e9d43fe
Jyotirmoy Sharma <136176621+jyotirmoy44@users.noreply.github.com>
2024-10-19 23:43:39 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 // Define function
 def call(String ProjectName, String ImageTag, String DockerHubUser) {
-sh "docker build -t ${DockerHubUser}/${ProjectName}:${Imagetag} ."
+sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
